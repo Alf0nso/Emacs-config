025848e9b3958267b25d1f5fd51cdfd571ff7192
Afonso Rafael <afonsorafael@sapo.pt>
2021-04-02 06:02:26 +0100

    changes related with roam

--- config.el
+++ config.el
@@ -172,7 +172,22 @@
 	  (tags "+Level=3+Anime")
 	  ))))
 
+(use-package org-roam
+  :ensure t
+  :hook (after-init . org-roam-mode)
+  :custom 
+  (org-roam-directory "~/Org/roam/")
+  :bind (:map org-roam-mode-map
+	 (("C-c n l" . org-roam)
+	  ("C-c n f" . org-roam-find-file)
+	  ("C-c n g" . org-roam-graph)
+	  ("C-c n b" . org-roam-switch-to-buffer)
+	  ("C-c n r" . org-roam-find-ref)
+	  ("C-c n d" . org-roam-find-directory))
+	 :map org-mode-map
+	 (("C-c n i" . org-roam-insert))))
 
+(setq org-roam-completion-system 'ivy)
 
 (require 'dired+)
 (use-package treemacs-icons-dired
